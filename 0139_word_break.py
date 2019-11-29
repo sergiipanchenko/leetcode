eebdb995bcfd1c25171eb710919cb22896d946aa
Jack_oS <s.panchenko@lookout.com>
2019-11-29 11:34:11 +0200

    accepted: 32ms 12.7MB

--- 0139_word_break.py
+++ 0139_word_break.py
@@ -1,17 +1,40 @@
 import unittest
 
 
-def word_break(s, word_dict):
-    word = ''
+# Accepted      32ms    12.7MB
+def word_break(s, wordDict):
+    check_list = [s]
+    seen_list = set()
 
-    while s:
-        word += s[0]
-        if word in word_dict:
-            # print(word)
-            word = ''
-        s = s[1:]
+    while check_list:
+        for word in wordDict:
+            check_word = check_list[0]
 
-    return False if word else True
+            if check_word.startswith(word):
+                remainder = check_word[len(word):]
+                if remainder == '':
+                    return True
+                if remainder not in seen_list:
+                    check_list.append(remainder)
+                    seen_list.add(remainder)
+        check_list.pop(0)
+
+    return False
+
+
+# def word_break(s, word_dict):
+#     pass
+
+
+# Wrong Answers:
+# def word_break(s, word_dict):
+#     word = ''
+#     while s:
+#         word += s[0]
+#         if word in word_dict:
+#             word = ''
+#         s = s[1:]
+#     return False if word else True
 
 
 class WordBreakTests(unittest.TestCase):
@@ -21,9 +44,13 @@ class WordBreakTests(unittest.TestCase):
         self.assertTrue(word_break('applepenapple', ['apple', 'pen']))
         self.assertFalse(word_break('catsandog', ['cats', 'dog', 'sand', 'and', 'cat']))
         self.assertTrue(word_break('cars', ['car', 'ca', 'rs']))
-        # TODO failures
         self.assertTrue(word_break('aaaaaaa', ['aaaa', 'aaa']))
+        # TODO Time Limit Exceeded
+        self.assertTrue(word_break('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab',
+                                   ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]))
 
 
 if __name__ == '__main__':
     unittest.main()
+    # print(word_break('leetcode', ['leet', 'code']))
+    # print(word_break('cars', ['car', 'ca', 'rs']))
