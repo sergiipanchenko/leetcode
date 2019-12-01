1c59843d17da58afefb3416f102d4185974faaa4
Jack_oS <s.panchenko@lookout.com>
2019-12-01 12:37:13 +0200

    accepted 28ms 12.8MB

--- 0139_word_break.py
+++ 0139_word_break.py
@@ -1,37 +1,52 @@
 import unittest
 
-
-# Accepted      32ms    12.7MB
+# Accepted      28ms   12.8MB
 def word_break(s, wordDict):
-    check_list = [s]
-    seen_list = set()
+    way_list = [0]*(len(s))
 
-    while check_list:
-        for word in wordDict:
-            check_word = check_list[0]
+    for each in wordDict:
+        if s.startswith(each):
+            way_list[len(each) - 1] += 1
 
-            if check_word.startswith(word):
-                remainder = check_word[len(word):]
-                if remainder == '':
-                    return True
-                if remainder not in seen_list:
-                    check_list.append(remainder)
-                    seen_list.add(remainder)
-        check_list.pop(0)
+    for i in range(1, len(s)):
+        reminder = s[i:]
+        for each in wordDict:
+            if way_list[i-1] and reminder.startswith(each):
+                way_list[i+len(each)-1] += 1
+
+    if way_list[-1]:
+        return True
 
     return False
 
 
-# def word_break(s, word_dict):
-#     pass
+# Accepted      32ms    12.7MB
+# def word_break_02(s, wordDict):
+#     check_list = [s]
+#     seen_list = set()
+#
+#     while check_list:
+#         for word in wordDict:
+#             check_word = check_list[0]
+#
+#             if check_word.startswith(word):
+#                 remainder = check_word[len(word):]
+#                 if remainder == '':
+#                     return True
+#                 if remainder not in seen_list:
+#                     check_list.append(remainder)
+#                     seen_list.add(remainder)
+#         check_list.pop(0)
+#
+#     return False
 
 
 # Wrong Answers:
-# def word_break(s, word_dict):
+# def word_break_01(s, wordDict):
 #     word = ''
 #     while s:
 #         word += s[0]
-#         if word in word_dict:
+#         if word in wordDict:
 #             word = ''
 #         s = s[1:]
 #     return False if word else True
@@ -45,12 +60,10 @@ class WordBreakTests(unittest.TestCase):
         self.assertFalse(word_break('catsandog', ['cats', 'dog', 'sand', 'and', 'cat']))
         self.assertTrue(word_break('cars', ['car', 'ca', 'rs']))
         self.assertTrue(word_break('aaaaaaa', ['aaaa', 'aaa']))
-        # TODO Time Limit Exceeded
-        self.assertTrue(word_break('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab',
+
+        self.assertFalse(word_break('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab',
                                    ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]))
 
 
 if __name__ == '__main__':
     unittest.main()
-    # print(word_break('leetcode', ['leet', 'code']))
-    # print(word_break('cars', ['car', 'ca', 'rs']))
